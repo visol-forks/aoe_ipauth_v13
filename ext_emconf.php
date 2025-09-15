ce8b6a9a54ac2bd37a87b8ae44aab14648f06aa4
lorenzulrich <lorenz.ulrich@visol.ch>
2025-09-15 18:49:47 +0200

    chore: Make installable in TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,19 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'IP Authentication',
     'description' => 'Authenticates users based on IP address settings',
-    'category' => 'services',
     'author' => 'Tomas Norre Mikkelsen',
     'author_email' => 'dev@aoe.com',
     'author_company' => 'AOE GmbH',
-    'state' => 'stable',
-    'version' => '5.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '12.4.0-12.4.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
-    ],
 ];
